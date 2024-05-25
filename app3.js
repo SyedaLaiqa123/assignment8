3bd54af96558fb8819b005da187b4a05e3449a78
Syeda Laiqa <157477259+SyedaLaiqa123@users.noreply.github.com>
2024-05-25 22:58:52 +0500

    Add files via upload

---
+++ app3.js
@@ -0,0 +1,11 @@
+let Age="My Age Is 19 Years Old";
+alert(Age)
+let A=15;
+alert("You have visited this site" + A + "times")
+let BirthYear="My Birth Year Is 2004   \n       ";
+document.write(BirthYear)
+// let Visitor="MR Anus";
+// let Product="Sunglasses";
+// let Quantity="5";
+// let store="VibeBuy";
+document.write("\nMR Anus ordered 5 Sunglasses on VibeBuy")
